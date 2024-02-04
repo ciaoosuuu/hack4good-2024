2664d2880909993c27e09f493c18fd21b2bf6117
vivienherq <heruoqingvivien@gmail.com>
2024-02-04 21:34:33 +0800

    Create blog page

---
+++ src/app/blog/page.js
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { db } from "../../firebase/config";
+import Entries from "../../components/blog/Entries";
+import classes from "./page.module.css";
+
+const Blog = () => {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const postsSnapshot = await db.collection("Posts").get();
+
+        const postsData = postsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(postsData);
+        setPosts(postsData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      Blog
+      {posts && <Entries entries={posts} classes={classes} />}
+    </div>
+  );
+};
+
+export default Blog;
