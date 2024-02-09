c88a663b0c5d451019bb4694c525c6a7104bd7e0
vivienherq <heruoqingvivien@gmail.com>
2024-02-09 17:24:44 +0800

    Fix attendance report page errors

--- src/app/reports/volunteer/[id]/page.js
+++ src/app/reports/volunteer/[id]/page.js
@@ -13,7 +13,6 @@ import {
   Flex,
 } from "@chakra-ui/react";
 import Image from "next/image";
-// import { useRouter } from "next/navigation";
 
 import ActivityCard from "../../../../components/activities/ActivityCard";
 import UserBadges from "../../../../components/gamify/UserBadges";
@@ -131,175 +130,167 @@ const VolunteerReport = ({ user, params }) => {
 
   return (
     <div className={classes["page_layout"]}>
-      {volunteer && <div>
-        <Box
-          style={{
-            padding: "0 0.5rem",
-            minWidth: "400px",
-          }}
-        >
-          <br />
-          <div>
+      {volunteer && (
+        <div>
+          <Box
+            style={{
+              padding: "0 0.5rem",
+              minWidth: "400px",
+            }}
+          >
+            <br />
             <div>
-              <div
+              <div>
+                <div
+                  style={{
+                    position: "absolute",
+                    marginTop: "30px",
+                    marginLeft: "-40px",
+                  }}
+                >
+                  <UserBadges userExp={user.exp_points} best={true} />
+                </div>
+                <ChakraImage
+                  style={{
+                    height: "400px",
+                    width: "100%",
+                    borderRadius: "30px",
+                    objectFit: "cover",
+                  }}
+                  src={volunteer.image}
+                  alt={volunteer.image}
+                />
+              </div>
+              <br />
+              <h1
                 style={{
-                  position: "absolute",
-                  marginTop: "30px",
-                  marginLeft: "-40px",
+                  fontSize: "26px",
+                  color: "maroon",
                 }}
               >
-                <UserBadges userExp={user.exp_points} best={true} />
-              </div>
-              <ChakraImage
-                style={{
-                  height: "400px",
-                  width: "100%",
-                  borderRadius: "30px",
-                  objectFit: "cover",
-                }}
-                src={volunteer.image}
-                alt={volunteer.image}
-              />
-            </div>
-            <br />
-            <h1
-              style={{
-                fontSize: "26px",
-                color: "maroon",
-              }}
-            >
-              {volunteer.name}
-            </h1>
-            <p>{volunteer.description}</p>
-            <br />
-            <Box
-              style={{
-                backgroundColor: "rgb(255,255,255,0.6",
-                padding: "1rem",
-                borderRadius: "20px",
-              }}
-            >
-              <Flex align={"center"}>
-                <Image
-                  src={require("../../../../resources/gem.png")}
-                  width={30}
-                  height={30}
-                  alt="Big At Heart"
-                  style={{ marginRight: "10px" }}
-                />
-                <h1>Total EXP: {volunteer.exp_points}</h1>
-              </Flex>
+                {volunteer.name}
+              </h1>
+              <p>{volunteer.description}</p>
               <br />
-              {volunteer.exp_points !== "0" && (
-                <UserBadges userExp={volunteer.exp_points} />
-              )}
-            </Box>
+              <Box
+                style={{
+                  backgroundColor: "rgb(255,255,255,0.6",
+                  padding: "1rem",
+                  borderRadius: "20px",
+                }}
+              >
+                <Flex align={"center"}>
+                  <Image
+                    src={require("../../../../resources/gem.png")}
+                    width={30}
+                    height={30}
+                    alt="Big At Heart"
+                    style={{ marginRight: "10px" }}
+                  />
+                  <h1>Total EXP: {volunteer.exp_points}</h1>
+                </Flex>
+                <br />
+                {volunteer.exp_points !== "0" && (
+                  <UserBadges userExp={volunteer.exp_points} />
+                )}
+              </Box>
+              <br />
+            </div>
             <br />
-            <Box
-              style={{ marginBottom: "8px" }}
-              className={classes["profile-button-dark"]}
-              onClick={() => router.push("/profile/edit-settings")}
-            >
-              Edit Settings
-            </Box>
-            <Box
-              className={classes["profile-button"]}
-              onClick={() => router.push("/profile/volunteer-preferences")}
-            >
-              Edit Skills & Preferences
-            </Box>
-          </div>
-          <br />
-        </Box>
-        <Box style={{ minWidth: "800px", maxWidth: "1000px" }}>
-          <br />
-          <div className={classes["main-selection-bar"]}>
-            <div
-              className={
-                selectedMainView === "Activities"
-                  ? `${classes["main-option-selected"]}`
-                  : `${classes["main-option-unselected"]}`
-              }
-              onClick={() => setSelectedMainView("Activities")}
-            >
-              Activities
-            </div>
-            <div
-              className={
-                selectedMainView === "Blog"
-                  ? `${classes["main-option-selected"]}`
-                  : `${classes["main-option-unselected"]}`
-              }
-              onClick={() => setSelectedMainView("Blog")}
-            >
-              Blog Posts
-            </div>
-          </div>
-          <br />
-          {selectedMainView === "Activities" && (
-            <>
-              <div className={classes["selection-bar"]}>
-                <div
-                  className={
-                    selectedView === "Signed Up"
-                      ? `${classes["option-selected"]}`
-                      : `${classes["option-unselected"]}`
-                  }
-                  onClick={() => setSelectedView("Signed Up")}
-                >
-                  Signed Up
-                </div>
-                <div
-                  className={
-                    selectedView === "Attended"
-                      ? `${classes["option-selected"]}`
-                      : `${classes["option-unselected"]}`
-                  }
-                  onClick={() => setSelectedView("Attended")}
-                >
-                  Attended
-                </div>
+          </Box>
+          <Box style={{ minWidth: "800px", maxWidth: "1000px" }}>
+            <br />
+            <div className={classes["main-selection-bar"]}>
+              <div
+                className={
+                  selectedMainView === "Activities"
+                    ? `${classes["main-option-selected"]}`
+                    : `${classes["main-option-unselected"]}`
+                }
+                onClick={() => setSelectedMainView("Activities")}
+              >
+                Activities
               </div>
+              <div
+                className={
+                  selectedMainView === "Attendance"
+                    ? `${classes["main-option-selected"]}`
+                    : `${classes["main-option-unselected"]}`
+                }
+                onClick={() => setSelectedMainView("Attendance")}
+              >
+                Attendance Report
+              </div>
+            </div>
+            <br />
+            {selectedMainView === "Activities" && (
+              <>
+                <div className={classes["selection-bar"]}>
+                  <div
+                    className={
+                      selectedView === "Signed Up"
+                        ? `${classes["option-selected"]}`
+                        : `${classes["option-unselected"]}`
+                    }
+                    onClick={() => setSelectedView("Signed Up")}
+                  >
+                    Signed Up
+                  </div>
+                  <div
+                    className={
+                      selectedView === "Attended"
+                        ? `${classes["option-selected"]}`
+                        : `${classes["option-unselected"]}`
+                    }
+                    onClick={() => setSelectedView("Attended")}
+                  >
+                    Attended
+                  </div>
+                </div>
 
-              {selectedMainView === "Activities" &&
-                selectedView === "Signed Up" && (
-                  <ul className={classes["grid_list_horizontal"]}>
-                    {signedUp &&
-                      signedUp
-                        .filter(
-                          (activity) =>
-                            activity.datetime_end.toDate() >= currentTimestamp
-                        )
-                        .map((activity) => (
-                          <ActivityCard key={activity.id} activity={activity} />
-                        ))}
-                  </ul>
-                )}
-              {selectedMainView === "Activities" &&
-                selectedView === "Attended" && (
-                  <ul className={classes["grid_list_horizontal"]}>
-                    {attended &&
-                      attended
-                        .filter(
-                          (activity) =>
-                            activity.datetime_end.toDate() < currentTimestamp
-                        )
-                        .map((activity) => (
-                          <ActivityCard
-                            key={activity.id}
-                            activity={activity}
-                            inProfile={true}
-                          />
-                        ))}
-                  </ul>
-                )}
-            </>
-          )}
-          {selectedMainView === "Blog" && (
-            <VolunteerPastYearAttendance volunteerId={id} />
-          )}
-        </Box>
-      </div>}
+                {selectedMainView === "Activities" &&
+                  selectedView === "Signed Up" && (
+                    <ul className={classes["grid_list_horizontal"]}>
+                      {signedUp &&
+                        signedUp
+                          .filter(
+                            (activity) =>
+                              activity.datetime_end.toDate() >= currentTimestamp
+                          )
+                          .map((activity) => (
+                            <ActivityCard
+                              key={activity.id}
+                              activity={activity}
+                            />
+                          ))}
+                    </ul>
+                  )}
+                {selectedMainView === "Activities" &&
+                  selectedView === "Attended" && (
+                    <ul className={classes["grid_list_horizontal"]}>
+                      {attended &&
+                        attended
+                          .filter(
+                            (activity) =>
+                              activity.datetime_end.toDate() < currentTimestamp
+                          )
+                          .map((activity) => (
+                            <ActivityCard
+                              key={activity.id}
+                              activity={activity}
+                              inProfile={true}
+                            />
+                          ))}
+                    </ul>
+                  )}
+              </>
+            )}
+            {selectedMainView === "Blog" && (
+              <VolunteerPastYearAttendance volunteerId={id} />
+            )}
+          </Box>
+        </div>
+      )}
     </div>
   );
 };
