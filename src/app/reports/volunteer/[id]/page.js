32d2a3945fb8d278e02affcefd0277bfaa149bca
Micaella <99943347+micaellaa@users.noreply.github.com>
2024-02-10 02:15:56 +0800

    Volunteer Reports page

--- src/app/reports/volunteer/[id]/page.js
+++ src/app/reports/volunteer/[id]/page.js
@@ -3,14 +3,14 @@
 import { useEffect, useState } from "react";
 import { db } from "../../../../firebase/config";
 import {
-  Box,
-  Image as ChakraImage,
-  Tabs,
-  TabList,
-  Tab,
-  TabPanels,
-  TabPanel,
-  Flex,
+	Box,
+	Image as ChakraImage,
+	Tabs,
+	TabList,
+	Tab,
+	TabPanels,
+	TabPanel,
+	Flex,
 } from "@chakra-ui/react";
 import Image from "next/image";
 
@@ -21,275 +21,303 @@ import withAuth from "../../../../hoc/withAuth";
 import VolunteerPastYearAttendance from "../../../../components/reports/VolunteerPastYearAttendance";
 
 const VolunteerReport = ({ user, params }) => {
-  const { id } = params;
-  const [volunteer, setVolunteer] = useState();
+	const { id } = params;
+	const [volunteer, setVolunteer] = useState();
 
-  const currentTimestamp = new Date();
-  const [selectedMainView, setSelectedMainView] = useState("Activities");
-  const [selectedView, setSelectedView] = useState("Signed Up");
-  const [signedUpIds, setSignedUpIds] = useState([]);
-  const [signedUp, setSignedUp] = useState([]);
-  const [attendedDetails, setAttendedDetails] = useState([]);
-  const [attended, setAttended] = useState([]);
+	const currentTimestamp = new Date();
+	const [selectedMainView, setSelectedMainView] = useState("Activities");
+	const [selectedView, setSelectedView] = useState("Signed Up");
+	const [signedUpIds, setSignedUpIds] = useState([]);
+	const [signedUp, setSignedUp] = useState([]);
+	const [attendedDetails, setAttendedDetails] = useState([]);
+	const [attended, setAttended] = useState([]);
 
-  useEffect(() => {
-    const fetchVolunteer = async () => {
-      try {
-        const userDoc = await db.collection("Users").doc(id).get();
-        if (userDoc.exists) {
-          const userData = userDoc.data();
-          setVolunteer(userData);
-          setSignedUpIds(userData.activities_signedup);
-          setAttendedDetails(userData.activities_attended);
-        }
-      } catch (error) {
-        console.error("Error fetching volunteer:", error);
-      }
-    };
-    fetchVolunteer();
-  }, []);
+	useEffect(() => {
+		const fetchVolunteer = async () => {
+			try {
+				const userDoc = await db.collection("Users").doc(id).get();
+				if (userDoc.exists) {
+					const userData = userDoc.data();
+					setVolunteer(userData);
+					setSignedUpIds(userData.activities_signedup);
+					setAttendedDetails(userData.activities_attended);
+				}
+			} catch (error) {
+				console.error("Error fetching volunteer:", error);
+			}
+		};
+		fetchVolunteer();
+	}, []);
 
-  useEffect(() => {
-    const fetchSignedUp = async () => {
-      if (!signedUpIds || !Array.isArray(signedUpIds)) {
-        return;
-      }
-      const promises = signedUpIds.map(async (activityId) => {
-        try {
-          const activityDoc = await db
-            .collection("Activities")
-            .doc(activityId)
-            .get();
-          console.log(activityDoc);
-          if (activityDoc.exists) {
-            return { id: activityId, ...activityDoc.data() };
-          } else {
-            console.log(`Activity with ID ${activityId} not found`);
-            return null;
-          }
-        } catch (error) {
-          console.error(
-            `Error fetching activity details for ${activityId}:`,
-            error
-          );
-          return null;
-        }
-      });
+	useEffect(() => {
+		const fetchSignedUp = async () => {
+			if (!signedUpIds || !Array.isArray(signedUpIds)) {
+				return;
+			}
+			const promises = signedUpIds.map(async (activityId) => {
+				try {
+					const activityDoc = await db
+						.collection("Activities")
+						.doc(activityId)
+						.get();
+					console.log(activityDoc);
+					if (activityDoc.exists) {
+						return { id: activityId, ...activityDoc.data() };
+					} else {
+						console.log(`Activity with ID ${activityId} not found`);
+						return null;
+					}
+				} catch (error) {
+					console.error(
+						`Error fetching activity details for ${activityId}:`,
+						error
+					);
+					return null;
+				}
+			});
 
-      const activityResults = await Promise.all(promises);
-      const activitySorted = activityResults
-        .filter(Boolean)
-        .filter(
-          (activity) => activity.datetime_end.toDate() >= currentTimestamp
-        )
-        .sort((activityA, activityB) => {
-          const startTimeA = activityA.datetime_start.toDate();
-          const startTimeB = activityB.datetime_start.toDate();
-          return startTimeA - startTimeB;
-        });
-      setSignedUp(activitySorted);
-    };
+			const activityResults = await Promise.all(promises);
+			const activitySorted = activityResults
+				.filter(Boolean)
+				.filter(
+					(activity) =>
+						activity.datetime_end.toDate() >= currentTimestamp
+				)
+				.sort((activityA, activityB) => {
+					const startTimeA = activityA.datetime_start.toDate();
+					const startTimeB = activityB.datetime_start.toDate();
+					return startTimeA - startTimeB;
+				});
+			setSignedUp(activitySorted);
+		};
 
-    const fetchAttended = async () => {
-      if (!attendedDetails || !Array.isArray(attendedDetails)) {
-        return;
-      }
-      const promises = attendedDetails.map(async (attended) => {
-        try {
-          const activityDoc = await db
-            .collection("Activities")
-            .doc(attended.activity_id)
-            .get();
-          if (activityDoc.exists) {
-            return {
-              id: attended.activity_id,
-              ...activityDoc.data(),
-            };
-          } else {
-            console.log(`Activity with ID ${attended.activity_id} not found`);
-            return null;
-          }
-        } catch (error) {
-          console.error(
-            `Error fetching activity details for ${attended.activity_id}:`,
-            error
-          );
-          return null;
-        }
-      });
+		const fetchAttended = async () => {
+			if (!attendedDetails || !Array.isArray(attendedDetails)) {
+				return;
+			}
+			const promises = attendedDetails.map(async (attended) => {
+				try {
+					const activityDoc = await db
+						.collection("Activities")
+						.doc(attended.activity_id)
+						.get();
+					if (activityDoc.exists) {
+						return {
+							id: attended.activity_id,
+							...activityDoc.data(),
+						};
+					} else {
+						console.log(
+							`Activity with ID ${attended.activity_id} not found`
+						);
+						return null;
+					}
+				} catch (error) {
+					console.error(
+						`Error fetching activity details for ${attended.activity_id}:`,
+						error
+					);
+					return null;
+				}
+			});
 
-      const activityResults = await Promise.all(promises);
-      const activitySorted = activityResults
-        .filter(Boolean)
-        .sort((activityA, activityB) => {
-          const startTimeA = activityA.datetime_start.toDate();
-          const startTimeB = activityB.datetime_start.toDate();
-          return startTimeB - startTimeA; // show latest first
-        });
+			const activityResults = await Promise.all(promises);
+			const activitySorted = activityResults
+				.filter(Boolean)
+				.sort((activityA, activityB) => {
+					const startTimeA = activityA.datetime_start.toDate();
+					const startTimeB = activityB.datetime_start.toDate();
+					return startTimeB - startTimeA; // show latest first
+				});
 
-      setAttended(activitySorted);
-    };
-    fetchSignedUp();
-    fetchAttended();
-  }, [volunteer]);
+			setAttended(activitySorted);
+		};
+		fetchSignedUp();
+		fetchAttended();
+	}, [volunteer]);
 
-  return (
-    <div className={classes["page_layout"]}>
-      {volunteer && (
-        <div>
-          <Box
-            style={{
-              padding: "0 0.5rem",
-              minWidth: "400px",
-            }}
-          >
-            <br />
-            <div>
-              <div>
-                <div
-                  style={{
-                    position: "absolute",
-                    marginTop: "30px",
-                    marginLeft: "-40px",
-                  }}
-                >
-                  <UserBadges userExp={user.exp_points} best={true} />
-                </div>
-                <ChakraImage
-                  style={{
-                    height: "400px",
-                    width: "100%",
-                    borderRadius: "30px",
-                    objectFit: "cover",
-                  }}
-                  src={volunteer.image}
-                  alt={volunteer.image}
-                />
-              </div>
-              <br />
-              <h1
-                style={{
-                  fontSize: "26px",
-                  color: "maroon",
-                }}
-              >
-                {volunteer.name}
-              </h1>
-              <p>{volunteer.description}</p>
-              <br />
-              <Box
-                style={{
-                  backgroundColor: "rgb(255,255,255,0.6",
-                  padding: "1rem",
-                  borderRadius: "20px",
-                }}
-              >
-                <Flex align={"center"}>
-                  <Image
-                    src={require("../../../../resources/gem.png")}
-                    width={30}
-                    height={30}
-                    alt="Big At Heart"
-                    style={{ marginRight: "10px" }}
-                  />
-                  <h1>Total EXP: {volunteer.exp_points}</h1>
-                </Flex>
-                <br />
-                {volunteer.exp_points !== "0" && (
-                  <UserBadges userExp={volunteer.exp_points} />
-                )}
-              </Box>
-              <br />
-            </div>
-            <br />
-          </Box>
-          <Box style={{ minWidth: "800px", maxWidth: "1000px" }}>
-            <br />
-            <div className={classes["main-selection-bar"]}>
-              <div
-                className={
-                  selectedMainView === "Activities"
-                    ? `${classes["main-option-selected"]}`
-                    : `${classes["main-option-unselected"]}`
-                }
-                onClick={() => setSelectedMainView("Activities")}
-              >
-                Activities
-              </div>
-              <div
-                className={
-                  selectedMainView === "Attendance"
-                    ? `${classes["main-option-selected"]}`
-                    : `${classes["main-option-unselected"]}`
-                }
-                onClick={() => setSelectedMainView("Attendance")}
-              >
-                Attendance Report
-              </div>
-            </div>
-            <br />
-            {selectedMainView === "Activities" && (
-              <>
-                <div className={classes["selection-bar"]}>
-                  <div
-                    className={
-                      selectedView === "Signed Up"
-                        ? `${classes["option-selected"]}`
-                        : `${classes["option-unselected"]}`
-                    }
-                    onClick={() => setSelectedView("Signed Up")}
-                  >
-                    Signed Up
-                  </div>
-                  <div
-                    className={
-                      selectedView === "Attended"
-                        ? `${classes["option-selected"]}`
-                        : `${classes["option-unselected"]}`
-                    }
-                    onClick={() => setSelectedView("Attended")}
-                  >
-                    Attended
-                  </div>
-                </div>
+	return (
+		<>
+			{volunteer && (
+				<div className={classes["page_layout"]}>
+					<Box
+						style={{
+							padding: "0 0.5rem",
+							minWidth: "400px",
+						}}
+					>
+						<br />
+						<div>
+							<div>
+								<div
+									style={{
+										position: "absolute",
+										marginTop: "30px",
+										marginLeft: "-40px",
+									}}
+								>
+									<UserBadges
+										userExp={user.exp_points}
+										best={true}
+									/>
+								</div>
+								<ChakraImage
+									style={{
+										height: "400px",
+										width: "100%",
+										borderRadius: "30px",
+										objectFit: "cover",
+									}}
+									src={volunteer.image}
+									alt={volunteer.image}
+								/>
+							</div>
+							<br />
+							<h1
+								style={{
+									fontSize: "26px",
+									color: "maroon",
+								}}
+							>
+								{volunteer.name}
+							</h1>
+							<p>{volunteer.description}</p>
+							<br />
+							<Box
+								style={{
+									backgroundColor: "rgb(255,255,255,0.6",
+									padding: "1rem",
+									borderRadius: "20px",
+								}}
+							>
+								<Flex align={"center"}>
+									<Image
+										src={require("../../../../resources/gem.png")}
+										width={30}
+										height={30}
+										alt="Big At Heart"
+										style={{ marginRight: "10px" }}
+									/>
+									<h1>Total EXP: {volunteer.exp_points}</h1>
+								</Flex>
+								<br />
+								{volunteer.exp_points !== "0" && (
+									<UserBadges
+										userExp={volunteer.exp_points}
+									/>
+								)}
+							</Box>
+							<br />
+						</div>
+						<br />
+					</Box>
+					<Box style={{ minWidth: "800px", maxWidth: "1000px" }}>
+						<br />
+						<div className={classes["main-selection-bar"]}>
+							<div
+								className={
+									selectedMainView === "Attendance"
+										? `${classes["main-option-selected"]}`
+										: `${classes["main-option-unselected"]}`
+								}
+								onClick={() =>
+									setSelectedMainView("Attendance")
+								}
+							>
+								Attendance Report
+							</div>
+							<div
+								className={
+									selectedMainView === "Activities"
+										? `${classes["main-option-selected"]}`
+										: `${classes["main-option-unselected"]}`
+								}
+								onClick={() =>
+									setSelectedMainView("Activities")
+								}
+							>
+								Activities
+							</div>
+						</div>
+						<br />
+						{selectedMainView === "Activities" && (
+							<>
+								<div className={classes["selection-bar"]}>
+									<div
+										className={
+											selectedView === "Signed Up"
+												? `${classes["option-selected"]}`
+												: `${classes["option-unselected"]}`
+										}
+										onClick={() =>
+											setSelectedView("Signed Up")
+										}
+									>
+										Signed Up
+									</div>
+									<div
+										className={
+											selectedView === "Attended"
+												? `${classes["option-selected"]}`
+												: `${classes["option-unselected"]}`
+										}
+										onClick={() =>
+											setSelectedView("Attended")
+										}
+									>
+										Attended
+									</div>
+								</div>
 
-                {selectedMainView === "Activities" &&
-                  selectedView === "Signed Up" && (
-                    <ul className={classes["grid_list_horizontal"]}>
-                      {signedUp &&
-                        signedUp.map((activity) => (
-                          <ActivityCard key={activity.id} activity={activity} />
-                        ))}
-                    </ul>
-                  )}
-                {selectedMainView === "Activities" &&
-                  selectedView === "Attended" && (
-                    <ul className={classes["grid_list_horizontal"]}>
-                      {attended &&
-                        attended
-                          .filter(
-                            (activity) =>
-                              activity.datetime_end.toDate() < currentTimestamp
-                          )
-                          .map((activity) => (
-                            <ActivityCard
-                              key={activity.id}
-                              activity={activity}
-                              inProfile={true}
-                            />
-                          ))}
-                    </ul>
-                  )}
-              </>
-            )}
-            {selectedMainView === "Attendance" && (
-              <VolunteerPastYearAttendance volunteerId={id} />
-            )}
-          </Box>
-        </div>
-      )}
-    </div>
-  );
+								{selectedMainView === "Activities" &&
+									selectedView === "Signed Up" && (
+										<ul
+											className={
+												classes["grid_list_horizontal"]
+											}
+										>
+											{signedUp &&
+												signedUp.map((activity) => (
+													<ActivityCard
+														key={activity.id}
+														activity={activity}
+													/>
+												))}
+										</ul>
+									)}
+								{selectedMainView === "Activities" &&
+									selectedView === "Attended" && (
+										<ul
+											className={
+												classes["grid_list_horizontal"]
+											}
+										>
+											{attended &&
+												attended
+													.filter(
+														(activity) =>
+															activity.datetime_end.toDate() <
+															currentTimestamp
+													)
+													.map((activity) => (
+														<ActivityCard
+															key={activity.id}
+															activity={activity}
+															inProfile={true}
+														/>
+													))}
+										</ul>
+									)}
+							</>
+						)}
+						{selectedMainView === "Attendance" && (
+							<VolunteerPastYearAttendance volunteerId={id} />
+						)}
+					</Box>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default withAuth(VolunteerReport);
